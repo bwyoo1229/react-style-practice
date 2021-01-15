68f32666879fa026d736195ed63480df72a5ade6
Byeonguk Yoo <bwyoo1229@gmail.com>
2021-01-16 00:18:41 +0900

    Styled-components 입문
    yarn add styled-components
    Tagged template literal을 이용해서 styled-components 적용

--- src/App.js
+++ src/App.js
@@ -1,67 +1,21 @@
-import Button from './components/Button';
 import React from 'react';
-import './App.scss';
+import styled, { css } from 'styled-components';
+
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${(props) => props.color};
+  border-radius: 50%;
+  ${(props) =>
+    props.huge &&
+    css`
+      width: 10rem;
+      height: 10rem;
+    `}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <div className="buttons">
-        <Button size="large">BUTTON</Button>
-        <Button>BUTTON</Button>
-        <Button size="small">BUTTON</Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" color="gray">
-          BUTTON
-        </Button>
-        <Button color="gray">BUTTON</Button>
-        <Button size="small" color="gray">
-          BUTTON
-        </Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" color="pink">
-          BUTTON
-        </Button>
-        <Button color="pink">BUTTON</Button>
-        <Button size="small" color="pink">
-          BUTTON
-        </Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" outline>
-          BUTTON
-        </Button>
-        <Button color="gray" outline>
-          BUTTON
-        </Button>
-        <Button size="small" color="pink" outline>
-          BUTTON
-        </Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" fullWidth>
-          BUTTON
-        </Button>
-        <Button size="large" color="gray" fullWidth>
-          BUTTON
-        </Button>
-        <Button
-          size="large"
-          color="pink"
-          fullWidth
-          onClick={() => {
-            console.log('클릭');
-          }}
-          onMouseMove={() => {
-            console.log('마우스 무브');
-          }}
-        >
-          BUTTON
-        </Button>
-      </div>
-    </div>
-  );
+  return <Circle color="blue" huge />;
 }
 
 export default App;
